82a70ef9c22b9c211152b17afd5221ab266217a9
Raj Choudhary <raaz02256@gmail.com>
2022-12-29 16:07:26 +0000

    Router-Pratice

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,10 @@
+// Write your JS code here
+const NotFound = () => (
+  <>
+    <div className="about-container">
+      <h1 className="heading">Not Found</h1>
+    </div>
+  </>
+)
+
+export default NotFound
